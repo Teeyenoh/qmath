bfd78e2c9540b2449e0afb6d0b209d5b8f477774
Teeyenoh <dylanfindlay1999@outlook.com>
2017-12-09 12:11:00 +0000

    Removed VS

--- QMath/QMath/Sequences.cpp
+++
@@ -1,7 +0,0 @@
-#include "Sequences.h"
-
-namespace QMath {
-	QLong Sequences::getFibonacciNumber(QLong n) {
-		return n == QLong(1) ? 1 : n == QLong(2) ? 2 : getFibonacciNumber(n - QLong(2)) + getFibonacciNumber(n - QLong(1));
-	}
-}
